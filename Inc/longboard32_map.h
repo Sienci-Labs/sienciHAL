5888a775318e952a2ce3c91603322562276671e9
andrew_marles <andrew@expatria.ca>
2023-09-19 14:34:29 -0700

    Commit updated initial pinout for SLB Black.

--- Inc/longboard32_map.h
+++ Inc/longboard32_map.h
@@ -98,7 +98,7 @@
 #define M3_STEP_PIN             5
 #define M3_DIRECTION_PORT       GPIOE
 #define M3_DIRECTION_PIN        10
-#define M3_LIMIT_PORT           GPIOB
+#define M3_LIMIT_PORT           GPIOE
 #define M3_LIMIT_PIN            6
 #define M3_ENABLE_PORT          GPIOC
 #define M3_ENABLE_PIN           7
@@ -107,8 +107,8 @@
 // Define ganged axis or A axis step pulse and step direction output pins.
 #if N_ABC_MOTORS == 2
 #define M4_AVAILABLE
-#define M4_STEP_PORT            GPIOE
-#define M4_STEP_PIN             6
+#define M4_STEP_PORT            GPIOB
+#define M4_STEP_PIN             10
 #define M4_DIRECTION_PORT       GPIOE
 #define M4_DIRECTION_PIN        11
 #define M4_LIMIT_PORT           GPIOE
@@ -137,8 +137,8 @@
 
 
 // Define flood and mist coolant enable output pins.
-#define COOLANT_FLOOD_PORT      GPIOD
-#define COOLANT_FLOOD_PIN       7
+#define COOLANT_FLOOD_PORT      GPIOB
+#define COOLANT_FLOOD_PIN       14
 #define COOLANT_MIST_PORT       GPIOB
 #define COOLANT_MIST_PIN        4
 #define COOLANT_OUTMODE         GPIO_BITBAND
@@ -150,7 +150,7 @@
 #define AUXOUTPUT2_PORT         GPIOC
 #define AUXOUTPUT2_PIN          1
 #define AUXOUTPUT3_PORT         GPIOC
-#define AUXOUTPUT3_PIN          4
+#define AUXOUTPUT3_PIN          8
 
 /*modbus direction pin*/
 #define AUXOUTPUT4_PORT         GPIOB
@@ -190,14 +190,17 @@
 //Run re-defined to Aux for Macro function.
 //#define CYCLE_START_PORT        GPIOC
 //#define CYCLE_START_PIN         11
-/* no safety door*/
+#if SAFETY_DOOR_ENABLE
+#define SAFETY_DOOR_PORT        GPIOD
+#define SAFETY_DOOR_PIN         7
+#endif
 
 
 #define CONTROL_INMODE          GPIO_BITBAND
 
 // Define probe switch input pin.
-#define PROBE_PORT              GPIOB
-#define PROBE_PIN               7
+#define PROBE_PORT         GPIOC     
+#define PROBE_PIN          4     
 
 #if I2C_STROBE_ENABLE
 #define I2C_STROBE_PORT         GPIOD
