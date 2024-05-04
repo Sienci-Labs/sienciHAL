a3fd69a0b8db9056a25deb517d638111cd6e492b
andrew_marles <andrew@expatria.ca>
2024-05-04 11:09:46 -0700

    Set up platformio to handle both SLB and SLB_EXT configurations.

--- extra_script.py
+++ extra_script.py
@@ -1,6 +1,8 @@
 Import("env")
 
-Import("env")
+env.Replace(PROGNAME="%s" % env.GetProjectOption("custom_prog_version")+"_"+env.GetProjectOption("grblhal_driver_version"))
+env.Replace(custom_board_name="%s" % env.GetProjectOption("custom_prog_version")+"_"+env.GetProjectOption("grblhal_driver_version"))
+
 # Custom HEX from ELF
 env.AddPostAction(
     "$BUILD_DIR/${PROGNAME}.elf",
