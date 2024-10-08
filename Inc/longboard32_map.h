c98ecc889b3e8acfb9c248373f72868277eef8bf
Sienci Labs <chris_thorogood@sienci.com>
2024-10-08 16:33:04 -0400

    Updates to get closer to parity and local building

--- Inc/longboard32_map.h
+++ Inc/longboard32_map.h
@@ -3,8 +3,6 @@
 
   Part of grblHAL
 
-  Copyright (c) 2024 Sienci Labs
-
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -17,6 +15,28 @@
 
   You should have received a copy of the GNU General Public License
   along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+
+  Copyright (C) Sienci Labs Inc.
+  
+   This file is part of the SuperLongBoard family of products.
+  
+   This source describes Open Hardware and is licensed under the "CERN-OHL-S v2"
+   You may redistribute and modify this source and make products using
+   it under the terms of the CERN-OHL-S v2 (https://ohwr.org/cern_ohl_s_v2.t). 
+   This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,
+   INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A 
+   PARTICULAR PURPOSE. Please see the CERN-OHL-S v2 for applicable conditions.
+   
+   As per CERN-OHL-S v2 section 4, should You produce hardware based on this 
+   source, You must maintain the Source Location clearly visible on the external
+   case of the CNC Controller or other product you make using this source.
+  
+   You should have received a copy of the CERN-OHL-S v2 license with this source.
+   If not, see <https://ohwr.org/project/cernohl/wikis/Documents/CERN-OHL-version-2>.
+   
+   Contact for information regarding this program and its license
+   can be sent through gSender@sienci.com or mailed to the main office
+   of Sienci Labs Inc. in Waterloo, Ontario, Canada.
 */
 
 #if N_ABC_MOTORS > 2
@@ -35,7 +55,6 @@
 #if SLB_EEPROM_ENABLE
 #undef I2C_ENABLE
 #undef EEPROM_ENABLE
-
 #define I2C_ENABLE 1
 #define EEPROM_ENABLE 2
 #define I2C_PORT 1
